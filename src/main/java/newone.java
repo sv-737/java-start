b49b5f2d48b7e2c0ceb85d2cc452ce0b4999ee69
viktorsergienko <737038@gmail.com>
2021-12-16 20:23:32 +0500

    try to use variables

--- src/main/java/newone.java
+++ src/main/java/newone.java
@@ -1,7 +1,10 @@
-public class newone{
-	public static void main (String[] args){
-	System.out.println("Руд");
-}
+public class newone {
+    public static void main(String[] args) {
+        System.out.println("Руд");
+        int L = 12;
+        int s = L * L;
+        System.out.println("площадь квадрата со стороной "+ L + "=" + (s));
+    }
 }
 
 
