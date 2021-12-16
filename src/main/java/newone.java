a966d3dfbfc77a87cbc03f0a9c2ae530307d45fb
viktorsergienko <737038@gmail.com>
2021-12-16 22:10:00 +0500

    завернул микрокод в функцию

--- src/main/java/newone.java
+++ src/main/java/newone.java
@@ -1,11 +1,15 @@
 public class newone {
     public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
         String text = "Автозапуск";
         System.out.println(text);
 
         double L = 7.6;
         double s = L * L;
-        System.out.println("площадь квадрата со стороной "+ L + "=" + (s));
+        System.out.println("площадь квадрата со стороной " + L + "=" + (s));
     }
 }
 
