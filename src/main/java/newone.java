f90e9cb9cf90663b518c94f03c9e7516a784f956
viktorsergienko <737038@gmail.com>
2021-12-17 11:54:31 +0500

    определил классы, создал объекты классов

--- src/main/java/newone.java
+++ src/main/java/newone.java
@@ -2,25 +2,26 @@ public class newone {
     public static void main(String[] args) {
         print("Привет");
         print("user");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s) );
 
-        double l = 5;
-        double a = 3;
-        double b = 12;
 
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(b, a) );
+        Rectangle r = new Rectangle(4,6);
+       // r.a = 3;
+        //r.b = 12;
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r) );
     }
 
     public static void print(String text) {
         System.out.println(text);
     }
 
-    public static double area(double len){
-        return len*len;
+    public static double area(Square s){
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b){
-        return a * b;
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 }
 
