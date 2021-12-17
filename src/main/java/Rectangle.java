af4ddd0aa821d28f0c6d21dee6c10fede6a08c89
viktorsergienko <737038@gmail.com>
2021-12-17 12:25:14 +0500

    завернул методы внутрь объектов, созданных классов

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -6,4 +6,8 @@ public class Rectangle {
         this.a=a;
         this.b=b;
     }
+
+    public double area(){
+        return this.a * this.b;
+    }
 }
--- src/main/java/newone.java
+++ src/main/java/newone.java
@@ -2,27 +2,20 @@ public class newone {
     public static void main(String[] args) {
         print("Привет");
         print("user");
-        Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s) );
+        Square s = new Square(4);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area() );
 
 
         Rectangle r = new Rectangle(4,6);
-       // r.a = 3;
+        //r.a = 3;
         //r.b = 12;
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r) );
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area() );
     }
 
     public static void print(String text) {
         System.out.println(text);
     }
 
-    public static double area(Square s){
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r){
-        return r.a * r.b;
-    }
 }
 
 
