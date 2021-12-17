c9ee953dd5148e4b0dd9699ea5de954b00f4413f
viktorsergienko <737038@gmail.com>
2021-12-17 11:28:14 +0500

    использовал несколько функций с разными параметрами

--- src/main/java/newone.java
+++ src/main/java/newone.java
@@ -1,15 +1,26 @@
 public class newone {
     public static void main(String[] args) {
-        print();
+        print("Привет");
+        print("user");
+
+        double l = 5;
+        double a = 3;
+        double b = 12;
+
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(b, a) );
     }
 
-    public static void print() {
-        String text = "Автозапуск";
+    public static void print(String text) {
         System.out.println(text);
+    }
 
-        double L = 7.6;
-        double s = L * L;
-        System.out.println("площадь квадрата со стороной " + L + "=" + (s));
+    public static double area(double len){
+        return len*len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }
 
